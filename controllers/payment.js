50ea3ba113f025f51b1171faded94e858820b2c9
MohammadAbuAwadd <147884198+abuawadd@users.noreply.github.com>
2025-09-01 04:51:42 +0300

    done2

--- controllers/payment.js
+++ controllers/payment.js
@@ -1,5 +1,5 @@
 exports.getPayment = (req,res) => {
-    res.send("<h1>PAYMENT Application (GET method)</h1>")
+    res.send("<h1>PAYMENT SuccessFull, Please Prepare Cash </h1>")
 }
 
 exports.postPayment = (req,res) => {
